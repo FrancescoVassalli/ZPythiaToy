2791f4d725f55c591a5b3b2461348abb1370920b
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-07-12 12:00:54 -0600

    made initial subtracted plot

--- histors/plotter.C
+++ histors/plotter.C
@@ -13,9 +13,9 @@ void plotDPhi(TFile* thisFile,std::vector<string> types){
 
 	unsigned count=0;
 	short colors[3]={kBlack,kRed,kBlue};
-	short styleTypes[4]={kFullCircle,kFullTriangleUp,kFullStar,kOpenDiamond};
+	short styleTypes[4]={kOpenCircle,kFullTriangleUp,kOpenStar,kFullDiamond};
 	unsigned typeCount=0;
-	TLegend* tl = new TLegend(.8,.1,.95,.3);
+	TLegend* tl = new TLegend(.7,.1,.9,.3);
 	for (std::vector<TH1F*>::iterator i = dphi_pt_plots.begin(); i != dphi_pt_plots.end(); ++i)
 	{
 		(*i)->GetYaxis()->SetRangeUser(10e-5,10e1);
@@ -45,9 +45,15 @@ void plotpT(TFile* thisFile,std::vector<string> types){
 	tc->SetLogy();
 	tc->SetLogx();
 
+	std::vector<TH1F*> subtracted_plots;
+	subtracted_plots.push_back((TH1F*)pt_plots[0]->Clone());
+	subtracted_plots.push_back((TH1F*)pt_plots[1]->Clone());
+	subtracted_plots.push_back((TH1F*)pt_plots[2]->Clone());
+
+
 	unsigned count=0;
 	short colors[3]={kBlack,kRed,kBlue};
-	short styleTypes[4]={kFullCircle,kFullTriangleUp,kFullStar,kOpenDiamond};
+	short styleTypes[4]={kOpenCircle,kFullTriangleUp,kOpenStar,kFullDiamond};
 	unsigned typeCount=0;
 	TLegend* tl = new TLegend(.7,.7,.9,.95);
 	for (std::vector<TH1F*>::iterator i = pt_plots.begin(); i != pt_plots.end(); ++i)
@@ -68,16 +74,29 @@ void plotpT(TFile* thisFile,std::vector<string> types){
 	minBias->SetLineColor(kMagenta-7);
 	minBias->Draw("same");
 	tl->AddEntry(minBias,"minbias","l");
-
 	tl->Draw();
 	tc->SaveAs("../plots/pT.pdf");
+
+	TCanvas *tc2 = new TCanvas();
+	count=0;
+	TLegend* tl2 = new TLegend(.7,.7,.9,.95);
+	for (std::vector<TH1F*>::iterator i = subtracted_plots.begin(); i != subtracted_plots.end(); ++i)
+	{
+		(*i)->Add(pt_plots[count+3],-1);
+		(*i)->SetLineColor(colors[count%3]);
+		if (count++==0)(*i)->Draw("e1");
+		else (*i)->Draw("e1 same");
+		tl2->AddEntry((*i),(*i)->GetName(),"p");
+	}
+	tl2->Draw();
 }
 
 void plotter(){
 	std::vector<string> types;
-	types.push_back("oof");
-	types.push_back("ooo");
-	types.push_back("in");
+	//types.push_back("inclusive_mpioff");
+	//types.push_back("inclusive_mpion");
+	types.push_back("forced_mpioff");
+	types.push_back("forced_mpion");
 	TFile *thisFile = new TFile("hists.root","READ");
 	plotDPhi(thisFile,types);
 	plotpT(thisFile,types);
